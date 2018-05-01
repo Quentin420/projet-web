b0038bd49a7f3ea6c88fc9675d9be4d337947817
Victor Botrel <botrel.victor@gmail.com>
2018-05-01 10:50:35 +0200

    insert

--- LinkECE/statut.php
+++ LinkECE/statut.php
@@ -1,14 +1,28 @@
-<?
-$host = 'localhost';
-$user = 'root';
-$pass = 'root'; 
-$db = 'linkece';
+<?php
+session_start();
 
-$id_user = $_SESSION['id_user'];
-$mysqli = new mysqli($host,$user,$pass,$db);
-$name = isset($_POST["statut"])?$_POST["statut"]:"";
-$sql="INSERT INTO post (id_user, descriptif) 
+// Check if user is logged in using the session variable
+if ( $_SESSION['logged_in'] != 1 ) {
+    $_SESSION['message'] = "You must log in before viewing your profile page!";
+    header("location: error.php");    
+}
+else {
+    // Makes it easier to read
+    $id_user = $_SESSION['id_user'];
+    $host = 'localhost';
+    $user = 'root';
+    $pass = 'root'; 
+    $db = 'linkece';
+
+    $id_user = $_SESSION['id_user'];
+    $mysqli = new mysqli($host,$user,$pass,$db);
+    $name = isset($_POST["statut"])?$_POST["statut"]:"";
+    $sql="INSERT INTO post (id_user, descriptif) 
         VALUES('$id_user','$name')";
-        $result = mysqli_query($db_handle, $sql);
 
-?>
+    if ($mysqli->query($sql) === TRUE) {
+        echo "New record created successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $mysqli->error;
+    }
+}?>

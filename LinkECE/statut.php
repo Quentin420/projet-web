04df917c0fe16ff4dd75dbaf019e2ee6fba3e4ef
Victor Botrel <botrel.victor@gmail.com>
2018-05-01 10:40:33 +0200

    supp

--- LinkECE/statut.php
+++ LinkECE/statut.php
@@ -3,12 +3,11 @@ $host = 'localhost';
 $user = 'root';
 $pass = 'root'; 
 $db = 'linkece';
+
 $id_user = $_SESSION['id_user'];
 $mysqli = new mysqli($host,$user,$pass,$db);
-
-$name = isset($_POST["nom_employée"])?$_POST["nom_employée"]:"";
-
-$sql="INSERT INTO post (id_user,descriptif) 
+$name = isset($_POST["statut"])?$_POST["statut"]:"";
+$sql="INSERT INTO post (id_user, descriptif) 
         VALUES('$id_user','$name')";
         $result = mysqli_query($db_handle, $sql);
 
--- login-system/db.php
+++
@@ -1,7 +0,0 @@
-<?php
-/* Database connection settings */
-$host = 'localhost';
-$user = 'root';
-$pass = 'root';
-$db = 'linkece';
-$mysqli = new mysqli($host,$user,$pass,$db) or die($mysqli->error);

b7d7510c5b40b64adcbf5cdd344accdce1a1c69d
PLGOUNOD <gounodpierrelouis@gmail.com>
2018-05-02 22:47:48 +0200

    Implémentation de LinkECEjob
    ajouts des fichiers elargiremploi et searchdataemploi

---
+++ LinkECE/elargiremploi.php
@@ -0,0 +1,153 @@
+<?php
+/* Displays user information and some useful messages */
+session_start();
+
+// Check if user is logged in using the session variable
+if ( $_SESSION['logged_in'] != 1 ) {
+    $_SESSION['message'] = "Vous n'êtes pas connecté!";
+    header("location: login-system/error.php");    
+}
+else {
+    // Makes it easier to read
+    $nom = $_SESSION['nom'];
+    $prenom = $_SESSION['prenom'];
+    $email = $_SESSION['email'];
+    $username = $_SESSION['username'];
+}
+?>
+
+
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Bienvenue sur LinkECE</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <script type="text/javascript">
+
+           $(function()
+           {
+            $(".search_button").click(function()
+            {
+                var search_word = $("#search_box").val();
+                var dataString = 'search_word='+search_word;
+
+                if (search_word=='')
+                {
+
+                }
+
+                else
+                {
+                    $.ajax({
+                        type: "GET",
+                        url: "searchingdataemploi.php",
+                        data: dataString,
+                        cache: false,
+                        beforeSend: function(html)
+                        {
+
+                            document.getElementById("insert_search").innerHTML='';
+                            $("#flash").show();
+                            $("#searchword").show();
+                            $("#flash").html('> Loading Results');
+
+                        },
+
+                        error: function(html){
+                         alert('error');
+                         },   
+
+                        success: function(html){
+                            $("#insert_search").show();
+                            $("#insert_search").append(html);
+                            $("#flash").hide();
+                            
+                        }
+                    });
+
+                }
+                return false;
+            });
+            });
+
+
+        
+        </script>
+
+
+        <style>    
+            /* Set black background color, white text and some padding */
+            footer {
+                background-color: #555;
+                color: white;
+                padding: 15px;
+            }
+            
+            #l{color: #4286f2}
+            #i{color: #e94333}
+            #n{color: #fdbe07}
+            #k{color: #4086f2}
+            #e1{color: #31aa52}
+            #c{color: #e94434}
+            #e2{color: #4286f2}
+            #people{font-size: 12px;}
+        </style>
+    </head>
+    <body>
+
+        <nav class="navbar navbar-inverse">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>                        
+                    </button>
+                    <style>.navbar-brand {font-weight: bold;}</style>
+                    <a class="navbar-brand" href="accueil.php">LinkECE</a>
+                </div>
+                <div class="collapse navbar-collapse" id="myNavbar">
+                    <ul class="nav navbar-nav">
+                        <li><a href="accueil.php"><span class="glyphicon glyphicon-home"></span> Accueil</a></li>
+                        <li><a href="reseau.php"><span class="glyphicon glyphicon-globe"></span> Réseau</a></li>
+                        <li><a href="chat/message.php"><span class="glyphicon glyphicon-envelope"></span> Messagerie</a></li>
+                        <li><a href="emplois.php"><span class="glyphicon glyphicon-search"></span> Emplois</a></li>
+                        <li><a href="notifications.php"><span class="glyphicon glyphicon-bell"></span> Notifications</a></li>
+                    </ul>
+
+
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="profil.php"><span class="glyphicon glyphicon-user"></span> <?= $prenom.' '.$nom ?> </a></li>
+                        <li><a href="login-system/logout.php"><span class="glyphicon glyphicon-log-out"></span> Déconnexion</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <div class="container text-center">   
+        
+            <div style="width:500px; margin:0 auto; margin-top:100px; background:#FFFFFF; padding:20px;">
+            <h1><span id="l">L</span><span id="i">i</span><span id="n">n</span><span id="k">k</span><span id="e1">E</span><span id="c">C</span><span id="e2">E</span><span id="people">'jobs</span></h1>
+            <form method="get" action="">
+               <input type="text" autocomplete=off name="search" id="search_box" class='search_box'/>
+               <input type="submit" value="Search" class="search_button" />
+            </form>
+            <br  />
+            <div id="searchword">
+            Search results for : <b><span class="searchword"></span></b></div>
+            <div id="flash"></div>
+            <ol id="insert_search" class="update" style="color: black; text-align: left;">
+            
+            </ol>
+        </div>
+
+            
+        </div>
+    </body>
+</html>
---
+++ LinkECE/searchingdataemploi.php
@@ -0,0 +1,46 @@
+<?php
+    include('connect.php');
+
+   
+    if(isset($_GET['search_word']))
+    {
+
+        $search_word=$_GET['search_word'];
+        $search_word_new=mysqli_real_escape_string($con, $search_word);
+        $search_word_fix=str_replace(" ","%",$search_word_new);
+        $sql=mysqli_query($con,"SELECT * FROM emploi WHERE (entreprise LIKE '%$search_word_fix%') OR (type_offre LIKE '%$search_word_fix%')ORDER BY id_emploi DESC LIMIT 10");
+        $count=mysqli_num_rows($sql);
+        if($count > 0)
+        {
+            while($row=mysqli_fetch_array($sql))
+            {
+            
+                $msg=$row['id_emploi'];
+                $emploi = mysqli_query($con, "SELECT * FROM `emploi` WHERE id_emploi='$msg'");
+                $emploi_fetch = mysqli_fetch_assoc($emploi);
+                $emploi_entreprise = $emploi_fetch['entreprise'];
+                $emploi_type_offre = $emploi_fetch['type_offre'];
+                $emploi_date = $emploi_fetch['date_emploi'];
+                $emploi_intitule = $emploi_fetch['intitule_offre'];
+                //display the message
+                echo "
+                            <div class='result'>
+                                <div class='text-con'>
+                                    <br/>
+                                    <p> Mise en ligne le : {$emploi_date} </p>
+                                    <p> Entreprise : {$emploi_entreprise} Type : {$emploi_type_offre}</p>
+                                    <p> Intitulé de l'offre : {$emploi_intitule} </p>
+                                    <a href='viewprofile.php?id_user=".$msg."'>Voir l'offre</a>
+                                    <br/>
+                                    <br/>
+                                </div>
+                            </div>
+                            <hr>";
+            }
+            }
+            else
+            {
+                echo "<li>Pas de résultats</li>";
+            }
+    }
+?>

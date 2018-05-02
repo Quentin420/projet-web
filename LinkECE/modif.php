2fbf211ab0299109e6facd8e945ba9c00bf6b5e1
Victor Botrel <botrel.victor@gmail.com>
2018-05-02 15:49:51 +0200

    modif photo/banniere/cv

--- LinkECE/modif.php
+++ LinkECE/modif.php
@@ -1,6 +1,7 @@
 <?php
 //header('Location: modifprofile.php');
 session_start();
+include('connect.php');
 
 // Check if user is logged in using the session variable
 if ( $_SESSION['logged_in'] != 1 ) {
@@ -9,36 +10,33 @@ if ( $_SESSION['logged_in'] != 1 ) {
 }
 else {
     // Makes it easier to read
-    $host = 'localhost';
-    $user = 'root';
-    $pass = 'root'; 
-    $db = 'linkece';
 
     $id_user = $_SESSION['id_user'];
-    $mysqli = new mysqli($host,$user,$pass,$db);
     
     
     // print_r($_FILES);die;
     
-    $avatar_path= $mysqli->real_escape_string('image/'.$_FILES['avatar']['name']);
-    $back_path= $mysqli->real_escape_string('image/'.$_FILES['background']['name']);
-    $cv_path= $mysqli->real_escape_string('image/'.$_FILES['cv']['name']);
+    $avatar_path= mysqli_real_escape_string($con,'img/'.$_FILES['avatar']['name']);
+    $back_path= mysqli_real_escape_string($con,'img/'.$_FILES['background']['name']);
+    $cv_path= mysqli_real_escape_string($con,'img/'.$_FILES['cv']['name']);
     
     //echo $cv_path; die;
     
-    if(preg_match("!image!",$_FILES['avatar']['type']) && preg_match("!image!",$_FILES['background']['type']) && preg_match("!image!",$_FILES['cv']['type']))
+    if((preg_match("!image!",$_FILES['avatar']['type']) || preg_match("!image!",$_FILES['background']['type'])) || preg_match("!image!",$_FILES['cv']['type']))
     {
         //echo $cv_path; die;
-        if(copy($_FILES['avatar']['tmp_name'],$avatar_path) && copy($_FILES['background']['tmp_name'],$back_path) && copy($_FILES['cv']['tmp_name'],$cv_path)){
+        if((copy($_FILES['avatar']['tmp_name'],$avatar_path) || copy($_FILES['background']['tmp_name'],$back_path)) || copy($_FILES['cv']['tmp_name'],$cv_path)){
             $_SESSION['avatar']=$avatar_path;
             $_SESSION['background']=$back_path;
             $_SESSION['cv']=$cv_path;
+            
             $sql="UPDATE users SET avatar='$avatar_path', background='$back_path', cv='$cv_path' WHERE id_user='$id_user'"; 
-            if ($mysqli->query($sql) === TRUE) {
+            if (mysqli_query($con,$sql) === TRUE) {
                 echo "New record created successfully";
                 header("location: profil.php");
+                echo "test3";
             } else {
-                echo "Error: " . $sql . "<br>" . $mysqli->error;
+                echo "Error: ";
             } 
         }
         else{

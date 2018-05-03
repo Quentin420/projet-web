0a25bf711e5a69d3199300025d283498ba65f804
Victor Botrel <botrel.victor@gmail.com>
2018-05-03 15:11:03 +0200

    nb like dans le bouton directement

--- LinkECE/accueil.php
+++ LinkECE/accueil.php
@@ -200,14 +200,13 @@ $dist_admin=$user_obj['admin'];
                                 <button type='button' class='btn btn-default btn-sm'><span class='glyphicon glyphicon-comment'></span> Commenter</button>
                                 <button type='button' class='btn btn-default btn-sm'><span class='glyphicon glyphicon-share'></span> Partager</button>";
                                 if($bool['nb']>0){
-                                    echo "<button class='btn btn-default btn-sm'><span class='glyphicon glyphicon-thumbs-up'></span> Déjà aimé</button>";
+                                    echo "<button class='btn btn-default btn-sm'><span class='glyphicon glyphicon-thumbs-up'></span> Déjà aimé (".$nb['nb'].")</button>";
                                 }else{
-                                    echo "<a href='like.php?id_post=". $post['id_post'] ."'><button type='button' class='btn btn-default btn-sm'><span class='glyphicon glyphicon-thumbs-up'></span> J'aime</button></a>";
+                                    echo "<a href='like.php?id_post=". $post['id_post'] ."'><button type='button' class='btn btn-default btn-sm'><span class='glyphicon glyphicon-thumbs-up'></span> J'aime (".$nb['nb'].")</button></a>";
                                 }
                                 echo "
                                 </div>
                                 </div>
-                                <p>Nb like : ". $nb['nb']."</p>
                             </div>
                         </div>  
                     </div>";

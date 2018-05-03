b93c39e35744b0803d7d6f83ab811a34eb58f4c1
Victor Botrel <botrel.victor@gmail.com>
2018-05-03 18:57:28 +0200

    Correctif affichage notif

--- LinkECE/notifications.php
+++ LinkECE/notifications.php
@@ -104,7 +104,7 @@ else {
                                 </div>
                             </div>
                         </div>
-                        </div>";
+                        ";
                 }
             
             

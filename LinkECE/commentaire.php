8960e8a50643c7bc75a26a4ca30453247f2834dc
Quentin Lemerle <qlaiche@gmail.com>
2018-05-03 17:29:42 +0200

    Fichier traitement des commentaires

---
+++ LinkECE/commentaire.php
@@ -0,0 +1,35 @@
+<?php
+session_start();
+include('connect.php');
+
+// Check if user is logged in using the session variable
+if ( $_SESSION['logged_in'] != 1 ) {
+    $_SESSION['message'] = "You must log in before viewing your profile page!";
+    header("location: error.php");    
+}
+else {
+    // Makes it easier to read
+    $host = 'localhost';
+    $user = 'root';
+    $pass = 'root'; 
+    $db = 'linkece';
+
+    $id_user = $_SESSION['id_user'];
+    if(isset($_GET['id_post']))
+    {
+         $n_post = $_GET['id_post'];
+         //$n_post=$_SESSION['id_post'];
+         $mysqli = new mysqli($host,$user,$pass,$db);
+         $commentaire = $mysqli->escape_string($_POST["commentaire"]);
+    
+         $sql="INSERT INTO commentaire (id_user, id_post, commenatire) VALUES ('$id_user', '$n_post', '$commentaire')"; 
+         if ($mysqli->query($sql) === TRUE) {
+            //$_SESSION['promotion']=$promotion;
+            echo "<script type='text/javascript'> document.location = 'accueil.php'; </script>";
+         } 
+        else{
+            echo "Error: " . $sql . "<br>" . $mysqli->error;
+        }
+    } 
+}
+?>

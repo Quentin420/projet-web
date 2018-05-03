d9b7220a9cfa104e7d745d6e568f5d1930d58516
Victor Botrel <botrel.victor@gmail.com>
2018-05-03 11:49:49 +0200

    admin - affichage par ordre alphabetique

--- LinkECE/admin.php
+++ LinkECE/admin.php
@@ -15,7 +15,7 @@ else {
     $username = $_SESSION['username'];
     $id_user = $_SESSION['id_user'];
 
-    $req = "SELECT * FROM users";
+    $req = "SELECT * FROM users ORDER BY nom ASC";
     $resultat = mysqli_query($con, $req);
 }
 //Requete speciale pour recuperer avatar et background du user logged
@@ -134,10 +134,7 @@ $dist_admin=$user_obj['admin'];
 
 
             <div class="row">
-                
-                
                 <div class="col-sm-12">      
-           
                 <h3 class="well">Supprimer un utilisateur</h3>
                             <?php
                     while($post = mysqli_fetch_array($resultat)){

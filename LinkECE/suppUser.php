a287ccc3319ea498f227ff92cca8e16d3de21d4c
Quentin Lemerle <qlaiche@gmail.com>
2018-05-03 11:16:21 +0200

    Blindage suppUser

--- LinkECE/suppUser.php
+++ LinkECE/suppUser.php
@@ -21,6 +21,12 @@ else{
         
         //Infos sur l'utilisateur dont on regarde le profil
         $user = mysqli_query($con, "DELETE FROM users WHERE (id_user = '$idviewed')");
+        $user1 = mysqli_query($con, "DELETE FROM relation WHERE id_user1 = '$idviewed' OR id_user2 = '$idviewed'");
+        $user2 = mysqli_query($con, "DELETE FROM post WHERE id_user = '$idviewed'");
+        $user3 = mysqli_query($con, "DELETE FROM messages WHERE user_from = '$idviewed' OR user_to = '$idviewed'");
+        $user4 = mysqli_query($con, "DELETE FROM like WHERE id_user = '$idviewed'");
+        $user5 = mysqli_query($con, "DELETE FROM commentaire WHERE id_user = '$idviewed'");
+        $user6 = mysqli_query($con, "DELETE FROM conversation WHERE user_one = '$idviewed' OR user_two = '$idviewed'");
         echo "<script type='text/javascript'> document.location = 'admin.php'; </script>";    
     }
 }
